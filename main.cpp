ade2dcc25b9783c6035659d8cba76a0d81fa6769
zhanchi5 <zhanchi5@users.noreply.github.com>
2017-10-30 12:36:38 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "stack.h"
+#include <thread>
+#include <mutex>
+#include <unistd.h>
+
+template<typename T>
+void producer(stack<T> &stack_, std::mutex& mtx){
+
+
+    for (;;){
+        int a = rand();
+        stack_.push(a);
+        std::cout<< a << "+" << "\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % (3) + 1));
+
+
+    }
+}
+template<typename T>
+void consumer(stack<T>& stack_,std::mutex& mtx){
+    for (;;){
+
+        try {
+            std::cout<<stack_.top() << "-" << "\n";
+            stack_.pop();
+
+
+        }
+        catch(...) {
+        }
+
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % (3) + 2));
+    }
+}
+
+int main(){
+    stack<int> st1;
+    std::mutex mt1;
+    std::thread t1(consumer<int>, std::ref(st1), std::ref(mt1));
+    std::thread t2(producer<int>, std::ref(st1), std::ref(mt1));
+    std::thread t3(consumer<int>, std::ref(st1), std::ref(mt1));
+    std::thread t4(producer<int>, std::ref(st1), std::ref(mt1));
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,135 @@
+
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <utility>
+#include <mutex>
+template <typename T>
+class stack
+{
+private:
+    void swap(stack<T> &) noexcept;
+
+    mutable std::mutex mtx;
+public:
+    stack() noexcept;
+    stack(stack<T> const &) /* strong */;
+    stack<T>& operator = (stack<T> const &)  noexcept;
+    size_t count() const noexcept;
+    void push(T const &) /* strong */;
+    void pop() /* strong */;
+    T top() /* strong */;
+
+
+
+private:
+    T* array_;
+    size_t array_size_;
+    size_t count_;
+};
+
+template <typename T>
+stack<T>::stack() noexcept : count_(0), array_size_(0), array_(nullptr)
+{}
+
+template <typename T>
+size_t stack<T>::count() const noexcept
+{
+    std::lock_guard<std::mutex> a(mtx);
+    return count_;
+
+}
+
+template <typename T>
+stack<T>::stack(stack<T> const& copy)
+{
+    std::lock_guard<std::mutex> a(copy.mtx);
+    T* temp = new T[copy.array_size_];
+    array_size_ = copy.array_size_;
+    count_ = copy.count_;
+    array_ = temp;
+    try
+    {
+        std::copy(copy.array_, copy.array_ + count_, array_);
+    }
+    catch( ... )
+    {
+        std::cerr << "Error! Try again!" << std::endl;
+        delete[] array_;
+    }
+}
+
+template <typename T>
+stack<T>& stack<T>::operator=(stack<T> const& other) noexcept
+{
+    std::lock(mtx,other.mtx);
+    if (this != &other)
+    {
+        stack<T> temp(other);
+        swap(temp);
+    }
+    mtx.unlock();
+    other.mtx.unlock();
+    return *this;
+}
+
+template <typename T>
+void stack<T>::swap(stack<T>& other) noexcept
+{
+    std::lock_guard<std::mutex> a(mtx);
+    std::swap(array_, other.array_);
+    std::swap(array_size_, other.array_size_);
+    std::swap(count_, other.count_);
+}
+
+template <typename T>
+void stack<T>::push(T const& value)
+{
+    std::lock_guard<std::mutex> a(mtx);
+    if (array_size_ == count_)
+    {
+        size_t size = array_size_;
+        if (size == 0)
+            size = 1;
+        else
+            size = array_size_ * 2;
+
+        T* temp = new T[size];
+        std::copy(array_, array_ + count_, temp);
+
+        array_size_ = size;
+        delete[] array_;
+        array_ = temp;
+    }
+
+    array_[count_] = value;
+    ++count_;
+}
+
+template <typename T>
+void stack<T>::pop()
+{
+    std::lock_guard<std::mutex> a(mtx);
+    if (count_ == 0)
+    {
+        throw "Stack is empty! Try again!\n";
+    }
+    else
+    {
+        --count_;
+    }
+}
+
+template <typename T>
+T stack<T>::top()
+{
+    std::lock_guard<std::mutex> a(mtx);
+    if (count_ == 0)
+    {
+        throw "Stack is empty!";
+    }
+    else
+    {
+        return array_[count_ - 1];
+    }
+}
